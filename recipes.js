45dc3b132a9319cac9422579ba5a8a3e4ffde230
LazarosZiskos <ziskoslaz@gmail.com>
2024-02-06 22:26:29 +0200

    finalised hamburger menu and recipes.js

--- recipes.js
+++ recipes.js
@@ -1,19 +1,53 @@
-async function spinnerRender() {
-    const spinnerWrapper = document.querySelector('.spinner__wrapper')
-  
-    spinnerWrapper.classList += " recipes__loading"
-    if (!recipe) {
-      recipe = await main()
-    }
-    spinnerWrapper.classList.remove('recipes__loading')
-  }
+const APP_ID = "e2c1665c";
+const APP_KEY = "84b7cf96ee4f65e1c0f08490f181851d";
 
-  
-  setTimeout(() => {
-    spinnerRender()
-  },5000)
-  
+function openMenu() {
+  document.body.classList += " menu--open";
+}
 
-  
+function closeMenu() {
+  document.body.classList.remove("menu--open");
+}
 
+const cardListEl = document.querySelector(".card__wrapper");
 
+let result;
+
+function renderHTML(result) {
+  return `<div class="card">
+    <img src="${result.recipe.image}" alt="food-image">
+    <div class="card__details">
+        <h2 class="card__details--title text-orange">${result.recipe.label}</h2>
+        </div>
+        <div>
+        <p class="card__details--para"><i>Cusine Type</i> : <b>${
+          result.recipe.cuisineType
+        }</b></p>  
+        <p class="card__details--para"><i>Dish Type</i> : <b>${
+          result.recipe.dishType
+        }</b></p>
+        <p class="card__details--para"><i>Calories</i> : <b>${result.recipe.calories.toFixed(
+          2
+        )}</b></p>
+        </div>  
+        <div class="card__details--link">
+        <a href=${result.recipe.url.replace(
+          /['"]+/g,
+          ""
+        )} target=_blank class="button margin-bottom" >View Full Recipe</a>
+        </div>
+        </div>
+        `;
+}
+
+async function searchChange(event) {
+  searchParam = event.target.value;
+
+  const response = await fetch(
+    `https://api.edamam.com/api/recipes/v2/?q=${searchParam}&app_id=${APP_ID}&app_key=${APP_KEY}&type=public`
+  );
+  const data = await response.json();
+
+  const results = data.hits;
+  cardListEl.innerHTML = results.map((result) => renderHTML(result)).join("");
+}
